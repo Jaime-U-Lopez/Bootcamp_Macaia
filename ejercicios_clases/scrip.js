df0e6ba58d7047b3e42b1d6901116b64e9839a96
Jaime Urrego <or4846@gmail.com>
2022-12-16 12:58:51 -0500

    se ingresan carpetas

---
+++ ejercicios_clases/scrip.js
@@ -0,0 +1,21 @@
+
+console.log("hola");
+document.write("hola")
+
+
+let i;
+for (i=0;i<=10;i++){
+  document.write(i)
+  
+  document.write("<br>")
+console.log(i)  
+}
+
+
+const factoria=5;
+let resulta=0;
+
+
+
+
+
